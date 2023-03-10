c723e34939487fc59a3bb45b4d2af74aeb84eeac
Tyler Lobos <tylobos22@gmail.com>
2023-03-09 22:00:06 -0500

    Co-authored-by: Zachery Beck <Zachery-Beck@users.noreply.github.com>

---
+++ menu.js
@@ -0,0 +1,20 @@
+// function to Grab the Entrees with AJAX
+function GetEntrees();{
+
+}
+
+// function to Grab the Appetizers with AJAX
+function GetApps();{
+
+}
+
+// function to Grab the Sides with AJAX
+function GetSides();{
+
+}
+
+// function to Grab the Entrees with AJAX
+function GetDrinks();{
+    
+}
+
