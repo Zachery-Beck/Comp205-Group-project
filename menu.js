6a289af91d31e737b8bb1f72eb5ab89220a49233
Tyler Lobos <tylobos22@gmail.com>
2023-03-23 17:47:00 -0400

    Co-authored-by: Zachery Beck <Zachery-Beck@users.noreply.github.com> Co-authored-by: Tristan Rutter  <TristanRutter@users.noreply.github.com>

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 // function to Grab the Entrees with AJAX
-function GetItems(Items, OutputID);{
+function GetItems(Items, OutputID){
     // declare local variables
     var xmlhttp;
 
@@ -29,7 +29,7 @@ function GetItems(Items, OutputID);{
             ( xmlhttp.status == 200 ))
         {
             // add the received html to the info section
-            document.getElementById( "takeout-lbar" ).innerHTML = xmlhttp.responseText;
+            document.getElementById( OutputID ).innerHTML = xmlhttp.responseText;
         }
     }
 
@@ -41,11 +41,56 @@ function GetItems(Items, OutputID);{
     //      the complete https:// on the file name if you are
     //      getting something from the same server. if you are
     //      not, you WILL need the fully qualified path.
-    xmlhttp.open( "GET", "OutputID", true );
+    xmlhttp.open( "GET", Items, true );
 
     // send the request to the server
     xmlhttp.send();
-
 }
 
 
+
+
+
+
+
+// start drag of item
+//      pass in the event that is triggering
+//      the drag
+function StartDrag( CurrentEvent )
+{
+    // sets the data type using MIME (first argument)
+    //      sets the source of the data (second argument)
+    //      the second argument is why the draggable items
+    //      need a unique id
+    CurrentEvent.dataTransfer.setData( "text/html", CurrentEvent.target.id );
+}
+     // allow a drop into the element
+function AllowDrop( CurrentEvent )
+{
+    // prevent the browser from handling the event
+    //      with its default handling. the default
+    //      is to not allow drag and drop. 
+    CurrentEvent.preventDefault();
+}
+        // handle dropping here
+function Drop( CurrentEvent, DestinationID )
+{
+    // this creates a variable called data
+    //      and initializes it in the same statement.
+    //      it gets the ID of the element that is being dragged.
+    var DataID = CurrentEvent.dataTransfer.getData( "text/html" );
+
+    // this moves the dragged element and adds it into
+    //      the destination element 
+    //document.getElementById( DestinationID ).appendChild( document.getElementById( DataID ));
+
+    // this copies the dragged element and adds it into
+    //      the destination element. the cloneNode method
+    //      makes a copy of the node instead of moving it
+    document.getElementById( DestinationID ).appendChild( document.getElementById( DataID ).cloneNode( true ));
+
+    // prevent the default handling which is to
+    //      not allow dropping
+    CurrentEvent.preventDefault();
+}
+        
