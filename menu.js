434fd3c4d681a38198e05b46bf811bafc02339f2
Tyler Lobos <tylobos22@gmail.com>
2023-03-22 21:27:08 -0400

    Co-authored-by: Zachery Beck <Zachery-Beck@users.noreply.github.com> Co-authored-by: Tristan Rutter  <TristanRutter@users.noreply.github.com>

--- menu.js
+++ menu.js
@@ -1,20 +1,51 @@
 // function to Grab the Entrees with AJAX
-function GetEntrees();{
+function GetItems(Items, OutputID);{
+    // declare local variables
+    var xmlhttp;
+
+    // create the request object depending on
+    //      the browser type
+    if ( window.XMLHttpRequest )
+    {
+        // code for IE7+, Firefox, Chrome, Safari, Opera
+        xmlhttp = new XMLHttpRequest;
+    }
+    else
+    {
+        // code for IE6 and earlier - need to use an
+        //      ActiveX object (yuck!)
+        xmlhttp = new ActiveXObject( "Microsoft.XMLHTTP" );
+    }
+
+    // set the callback function. this function inserts the html that
+    //      is received into the "info" section of the page. It is
+    //      a callback function because the XMLHttpRequest object
+    //      calls this function when the ready state changes and it
+    //      is provided by the user of the object.
+    xmlhttp.onreadystatechange = function ()
+    {
+        // check for a good return
+        if (( xmlhttp.readyState == 4 ) &&
+            ( xmlhttp.status == 200 ))
+        {
+            // add the received html to the info section
+            document.getElementById( "takeout-lbar" ).innerHTML = xmlhttp.responseText;
+        }
+    }
+
+    // create the request. this creates a HTTP GET request
+    //      for the file name in the second parameter (argument).
+    //      the final argument is whether the request is
+    //      synchronous or asynchronous. generally use
+    //      asynchronous (true). also note that you don't need
+    //      the complete https:// on the file name if you are
+    //      getting something from the same server. if you are
+    //      not, you WILL need the fully qualified path.
+    xmlhttp.open( "GET", "OutputID", true );
+
+    // send the request to the server
+    xmlhttp.send();
 
 }
 
-// function to Grab the Appetizers with AJAX
-function GetApps();{
-
-}
-
-// function to Grab the Sides with AJAX
-function GetSides();{
-
-}
-
-// function to Grab the Entrees with AJAX
-function GetDrinks();{
-    
-}
 
