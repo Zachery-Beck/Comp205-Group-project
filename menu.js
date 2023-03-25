5946f32a4c68bb58af3783e49965e7c5ad89cfcd
Tyler Lobos <tylobos22@gmail.com>
2023-03-24 23:31:47 -0400

    Co-authored-by: Tristan Rutter  <TristanRutter@users.noreply.github.com> Co-authored-by: Zachery Beck <Zachery-Beck@users.noreply.github.com>

--- menu.js
+++ menu.js
@@ -50,7 +50,7 @@ function GetItems(Items, OutputID){
 // END OF AJAX //
 
 
-/////
+
 
 
 
@@ -71,99 +71,124 @@ function AllowDrop( CurrentEvent )
     // prevent the browser from handling the event
     //      with its default handling. the default
     //      is to not allow drag and drop. 
-    CurrentEvent.preventDefault();
+    CurrentEvent.preventDefault(); 
 }
-        // handle dropping here
+
+
+
+//drop function 
 function Drop( CurrentEvent, DestinationID)
 {
-    // this creates a variable called data
-    //      and initializes it in the same statement.
-    //      it gets the ID of the element that is being dragged.
+    //creates variable data
     var DataID = CurrentEvent.dataTransfer.getData( "text/html" );
 
-    // this copies the dragged element and adds it into
-    //      the destination element. the cloneNode method
-    //      makes a copy of the node instead of moving it
-    document.getElementById( DestinationID ).appendChild( document.getElementById( DataID ).cloneNode( true ));
 
-    // prevent the default handling which is to
-    //      not allow dropping
+    // create counter 
+    var Counter = Number( document.getElementById(DataID).getAttribute("count")); 
+    if (Counter < 1) {
+   
+        //moves dragged element + add
+   
+    document.getElementById( DestinationID ).appendChild( document.getElementById( DataID ).cloneNode( true ));
+    
+    
     CurrentEvent.preventDefault();
 
-        // get the grade element and point value
-        var SourceGrade = document.querySelector( "div[id=\"leftlist\"] div[id=\"" + DataID + "\"]" )
-        var PointValue = SourceGrade.getAttribute( "price" );
+    // create a count
+    var Count = Number( document.getElementById(DataID).getAttribute("count"));
+    }
+    // variable to change DIV
+    var element = document.getElementById(DataID);
 
-        // get the grade element and point value
-        var DestinationGrade = document.querySelector( "div[id=\"rightlist\"] div[id=\"" + DataID + "\"]" );
     
-        // check for no grade found
-        if ( null == DestinationGrade )
-            {
-                // // add a new grade
-                // DestinationGrade = document.createElement( "DIV" );
+    amount.setAttribute("id", "itemPrice" + DataID)
+// If new add to list
+    if (Count< 1) {
+
+        element.classList.remove("item");
+        element.classList.add("menuItem");
+        Count = Number( element.getAttribute("count"))+1;
+        element.setAttribute("count",Count);
+        amount.innerHTML = "Total: " + Count;
+        element.addEventListener( "ondblclick", function () { RemoveItems(DataID); } );
+        
+
+    }
+// if already there add to count
+    else {
+        Count = Number( element.getAttribute("count")) + 1;
+        document.getElementById(("ItemPrice"+ DataID)).innerHTML = "Total: " + Count;
+        element.setAttribute("count", Count);
+        
+    }
+        FindPrice();
+    }
+//end of drop function
+
     
-                // set the element attributes
-                DestinationGrade.setAttribute( "id", Item );
-                DestinationGrade.setAttribute( "class", "item" );        
-                DestinationGrade.setAttribute( "price", PointValue );
-                DestinationGrade.setAttribute( "data-count", 1 );
-                DestinationGrade.addEventListener( "dblclick", function () { RemoveItem( DataID ); } );
-                // add the text for the grade in the calculator
-                DestinationGrade.innerHTML = DataID + "<br />(1)";
+    function FindPrice() {
+
+        // get the list right
+        var List = document.querySelectorAll( "div[id=\"rightlist\"] div" );
+        
+        //create price constant variable
+        var Price = 0;
+        
+            // run for each item in the cart
+    for( var i=0; i<List.length; i++ )
+    {
+        // variable to store how many of each item there is
+        var Count = 0;
+        var amount = List[i].getAttribute( "count" );
+        Count += Number(amount);
+       
+        // get the price of item for position
+        var iprice = List[i].getAttribute( "price" );
+        
+        // add the price to the total
+        for (k=0; k<Count; k++){
+        Price += Number(iprice);
+        }
+    }
+        
+       
+        // select the box to get tot
+        var TotalPrice = document.querySelector( "p[id=\"total\"]");
+        TotalPrice.innerHTML = "Total is $" + Price;
     
-                // add the element to the destination div
-                document.querySelector( "div[id=\"rightlist\"]" ).appendChild( DestinationGrade );
-            }
-        else
-            {
-                // update existing grade 
-                var Count = Number( DestinationGrade.getAttribute( "data-count" )) + 1;
-                DestinationGrade.addEventListener( "dblclick", function () { RemoveItem( DataID ); } );
-                DestinationGrade.setAttribute( "data-count", Count );
-                DestinationGrade.innerHTML = DataID + "<br />(" + Count + ")";    
-            }
-}
-            // function RemoveItem(DataID) {
-            //     var Result = window.confirm( "Remove " + DataID );
-                
-            //     if (Result == true)
-            //     {
-            //         const element = document.getElementById(DataID);
-            //         element.remove();
+        // check if the length is 0 then set output
+        if(List.length == 0) {
+            TotalPrice.innerHTML = "Total is $0";
+        }
+    
+    }
+    
+    function RemoveItem(DataID) {
+        // prompt the user
+    var Result = window.confirm( "Do you want to remove " + DataID );
 
-            //     }
-            // }
-            
+    // remove if clicked
+    //Could not get this to work in the double click
+    if ( Result == true )
+    {
+        // get the count number and create an element to adjust variables
+        var Count = Number( document.getElementById(DataID).getAttribute("count"));
+        const element = document.getElementById(DataID);
+        
+        if (Count > 1){
+            Count = Number( element.getAttribute("count")) - 1;
+        document.getElementById("itemPrice" + DataID).innerHTML = "Total: " + Count;
+        element.setAttribute("count", Count );   
+        }
+        // remove last
+        else {
+            element.remove();
+        }
 
-            function RemoveItem( Item ) {
-                // prompt the user
-                var Result = window.confirm( "Remove the Item?" );
-                // process based on the result
-                if ( Result == true )
-                {
-                // get the existing grade in the calculator
-                var DestinationGrade = document.querySelector( "div[id=\"rightlist\"] div[id=\"" + Item + "\"]" );
-                // drop the count down by one
-                var Count = Number( DestinationGrade.getAttribute( "data-count" )) - 1;
-                // see if it is the last
-                if ( 0 == Count )
-                {
-                // remove the node
-                DestinationGrade.parentNode.removeChild( DestinationGrade );
-                }
-                else
-                {
-                // update the count
-                DestinationGrade.setAttribute( "data-count", Count );
-                DestinationGrade.innerHTML = Item + "<br />(" + Count + ")";
-                }
-                }
-                ComputeGPA();
-                }
-                
-
-
+    }
+    // call back to function for finding price
+    FindPrice();
+    }
 
 
 
